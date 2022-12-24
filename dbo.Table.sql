ad3e83c70e4f9c80477f8d5ea7976a22da256cb2
Mar7ins <84845448+AlbertoMar7ins@users.noreply.github.com>
2022-12-24 14:30:21 -0300

    sqlFiles

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [cliente] VARCHAR(50) NULL, 
+    [data] DATE NULL, 
+    [hora] VARCHAR(10) NULL, 
+    [servico] VARCHAR(50) NULL
+)
